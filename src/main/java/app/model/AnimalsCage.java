daa2e18c070e4d14c6f37b8b13be3448b97109a2
Данил <ladnopot@gmail.com>
2025-01-12 02:53:10 +0300

    #1 SpringBeam first version

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,24 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
+    private final Timer timer;
     private Animal animal;
+
+    public Timer getTimer(){
+        return timer;
+    }
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
